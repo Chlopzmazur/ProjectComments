4a6b53cb59d354053e3a402d46fb4e41ea9ea7b7
Wiktor <wiktorszczepaniak1@gmail.com>
2017-07-26 13:23:19 +0200

    added some comments

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+//this will input the prompt asking the name
+var name = prompt('Enter your name');
+// made the alert using variable with name
+alert('Hello, ' + name);
+// this here will input the script in the console
+console.log('Hello, ' + name);
